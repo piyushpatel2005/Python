47ef0c4924765d7808ec73f227c2cfdbf97777ae
Piyush Patel <piyushpatel2005@gmail.com>
2019-05-20 20:42:04 -0400

    make listing and about page dynamic

--- web-programming-harvard/django/real_estate_project/listings/views.py
+++ web-programming-harvard/django/real_estate_project/listings/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Listing
 
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-    paginator = Paginator(listings, 3)
+    paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     context = {
@@ -17,7 +17,11 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
