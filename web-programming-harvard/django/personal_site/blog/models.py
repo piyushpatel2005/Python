2982e1394b4005054539f8da532006cf6c8dec98
Piyush Patel <piyushpatel2005@gmail.com>
2019-10-23 16:11:04 -0400

    created templates for blog pages

--- web-programming-harvard/django/personal_site/blog/models.py
+++ web-programming-harvard/django/personal_site/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create special published manager that gets only published blogs
 class PublishedManager(models.Manager):
@@ -29,8 +30,21 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[
+                            self.publish.year,
+                            self.publish.month,
+                            self.publish.day,
+                            self.slug
+                        ])
+
+    def __str__(self):
+        return self.title
+
     class Meta:
         ordering = ('-publish',) # sort the results in descending order publish field
 
-        def __str__(self):
-            return self.title
+        
+
+    
---
+++ web-programming-harvard/django/personal_site/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog' # used to organize urls by application
+
+urlpatterns = [
+    # You can se re_path() to define complex URL patterns with Python regular expressions
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_details, name='post_detail'),
+]
